83297c3fd6ce3bbc8f6212ca5ee6327442828105
villar88 <villar88@gmail.com>
2020-12-02 14:00:52 -0400

    Update dashboardgrap.blade.php

--- resources/views/dashboardgrap.blade.php
+++ resources/views/dashboardgrap.blade.php
@@ -210,6 +210,7 @@
 
             'use strict';
             var ctx1 = document.getElementById('chartBar1').getContext('2d');
+            ctx1.clearRect(0, 0, canvas.width, canvas.height)
             new Chart(ctx1, {
                 type: 'bar',
                 data: {
