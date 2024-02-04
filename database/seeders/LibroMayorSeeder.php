13cb9cb559cc1553e26dd9f5b6ee08487c53d2e4
Juan Boscan <boscanjuan805@gmail.com>
2024-02-04 18:05:39 -0400

    seeder

---
+++ database/seeders/LibroMayorSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\LibroMayor;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+
+class LibroMayorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $fecha = Carbon::now()->format('Y-m-d');;
+
+        $libros = [
+            [
+                'cuenta' => 'Ingresos',
+                'ultimo_saldo' => $fecha,
+                'saldo_inicial' => 0,
+                'icon' => 'fa fa-line-chart',
+                'saldo' => 0,
+
+            ],
+            [
+                'cuenta' => 'Cuentas por cobrar',
+                'ultimo_saldo' => $fecha,
+                'saldo_inicial' => 0,
+                'icon' => 'fa fa-line-chart',
+                'saldo' => 0,
+
+            ],
+            
+        ];
+        LibroMayor::insert($libros);   
+     }
+}
