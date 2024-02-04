bec84395cdd548d10f89f699bb3e837ca441ea66
Juan Boscan <boscanjuan805@gmail.com>
2024-02-04 18:06:26 -0400

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleTableSeeder::class,
             UserTableSeeder::class,
-            DivisaSeeder::class
+            DivisaSeeder::class,
+            LibroMayorSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
