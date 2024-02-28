f89c9e7ba1442fb5d693e8c1894118acf864edf2
AngelMolinaO <angeln@h2d.dev>
2024-02-28 10:53:38 -0400

    actualizar el scraping con puro php

--- app/Console/Commands/scraping.php
+++ app/Console/Commands/scraping.php
@@ -3,7 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Models\Divisa;
-use GuzzleHttp\Promise\Promise;
+use Goutte\Client;
+use Symfony\Component\HttpClient\HttpClient;use GuzzleHttp\Promise\Promise;
 use Illuminate\Console\Command;
 use Illuminate\Support\Sleep;
 
@@ -30,14 +31,33 @@ class scraping extends Command
     public function handle()
     {
 
-        ///fetch a url extern
-        $client = new \GuzzleHttp\Client();
-        $response = $client->get('http://0.0.0.0:3000/traer/usd');
-        $number = $response->getBody();
-        echo  $number;
-        $cop = Divisa::where('name', "COP")->first();
-        $cop->tasa = $number;
-        $cop->save();
+        // ///fetch a url extern
+        // $client = new \GuzzleHttp\Client();
+        // $response = $client->get('http://0.0.0.0:3000/traer/usd');
+        // $number = $response->getBody();
+        // echo  $number;
+        // $cop = Divisa::where('name', "COP")->first();
+        // $cop->tasa = $number;
+        // $cop->save();
+        $client = new Client;
+        $url = "https://www.google.com/finance/quote/USD-COP?sa=X&ved=2ahUKEwj6-8DjoM6EAxWNmYQIHfA_C78QmY0JegQIBxAv";
+
+        $crawler = $client->request('GET', $url);
+        $numberCop = $crawler->filter('.fxKbKc')->each(function ($node) {
+            return  $node->text();
+        });
+
+        if(!empty($numberCop)){
+            echo $numberCop[0] . " ";
+            $paragraphs = $numberCop[0];
+            $numberCopLast = str_replace(',', '', $paragraphs);
+            $number = floatval($numberCopLast);
+            echo  $number. "Se actualizo el cop";
+            $cop = Divisa::where('name', "COP")->first();
+            $cop->tasa = $number;
+            $cop->save();
+        }
+
 
         
 
--- app/Console/Commands/updateVes.php
+++ app/Console/Commands/updateVes.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Divisa;
 use Goutte\Client;
 use Illuminate\Console\Command;
+use Symfony\Component\HttpClient\HttpClient;
 
 class updateVes extends Command
 {
@@ -27,14 +28,16 @@ class updateVes extends Command
      */
     public function handle()
     {
-        echo "/";
+        $client = new Client(HttpClient::create(['verify_peer' => false]));
+        $crawler = $client->request('GET', 'https://www.bcv.org.ve/seccionportal/tipo-de-cambio-oficial-del-bcv');
+        $paragraphs = $crawler->filter('strong')->each(function ($node) {
+            return  $node->text();
+        });
 
-        $client = new \GuzzleHttp\Client();
-        $response = $client->get('http://localhost:3000/traer/bs');
-        $number = $response->getBody();
-        echo  $number;
+        $ultimoValor = !empty($paragraphs) ? (end($paragraphs) !== null ? str_replace(',', '.', end($paragraphs)) : 0) : 0;
+        echo $ultimoValor . "se actualizo el precio del BS";
         $bs = Divisa::where('name', "Bs")->first();
-        $bs->tasa = $number;
+        $bs->tasa = floatval($ultimoValor);
         $bs->save();
     }
 }
